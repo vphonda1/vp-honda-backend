a178140fb5d302a62e727087e80bf5d3aa25cba9
Raja <vp honda>
2026-04-26 17:25:22 +0530

    Fix: solve error

--- server.js
+++ server.js
@@ -39,7 +39,9 @@ app.use('/api/oldbikes',         require('./routes/oldbikes'));
 app.use('/api/quotations',       require('./routes/quotations'));
 app.use('/api',                  require('./routes/dataImport'));
 app.use('/api/service-data',     require('./routes/servicedata'));
+
 app.use('/api/follow-ups',       require('./routes/followups'));
+
 
 // ⭐ NEW: Salary management
 app.use('/api/salaries',         require('./routes/salaries'));
