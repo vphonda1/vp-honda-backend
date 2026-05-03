76a8dcbb4ce54f91462a4118b9a2d3f294600718
Raja <vp honda>
2026-05-03 09:24:22 +0530

    Fix: notification

--- render-build.sh
+++ render-build.sh
@@ -4,8 +4,4 @@ set -o errexit
 echo "📦 Installing dependencies..."
 npm install
 
-#!/bin/bash
-apt-get update && apt-get install -y libnss3 libatk-bridge2.0-0 libdrm-dev libxkbcommon-dev libgbm-dev libasound-dev
-
-
 echo "✅ Build completed"
